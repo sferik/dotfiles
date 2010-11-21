e2c7be2ed3c6ce8e65ff096ec9d995b9eef3b775
Erik Michaels-Ober <sferik@gmail.com>
2010-11-20 19:16:09 -0800

    Add 186 alias

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,6 @@
 alias ll="/bin/ls -AFGHhl"
 alias ls="/bin/ls -FGH"
+alias 186="rvm use 1.8.6"
 alias 187="echo Yeah, and you don\'t stop. Cause it\'s 187 on an undercover cop.; rvm use 1.8.7"
 alias 192="rvm use 1.9.2"
 alias ironruby="rvm use ironruby"
