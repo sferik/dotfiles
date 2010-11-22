8f8ec3b99e1c3a64b052a2e45cf35555a6dbd36d
Erik Michaels-Ober <sferik@gmail.com>
2010-11-22 09:19:55 -0800

    Create rvm aliases in a loop

--- aliases.sh
+++ aliases.sh
@@ -3,10 +3,8 @@ alias ls="/bin/ls -FGH"
 alias 186="rvm use 1.8.6"
 alias 187="echo Yeah, and you don\'t stop. Cause it\'s 187 on an undercover cop.; rvm use 1.8.7"
 alias 192="rvm use 1.9.2"
-alias ironruby="rvm use ironruby"
-alias jruby="rvm use jruby"
-alias macruby="rvm use macruby"
-alias maglev="rvm use maglev"
-alias mput="rvm use mput"
-alias rbx="rvm use rubinius"
-alias ree="rvm use ree"
+rubies=(ironruby jruby macruby maglev mput rbx ree)
+for ruby in ${rubies[@]}
+do
+  alias $ruby="rvm use $ruby"
+done
