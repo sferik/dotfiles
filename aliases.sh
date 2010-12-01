dea3f89e0086090bf32e3ea0aad8b2e14434fcb9
Erik Michaels-Ober <sferik@gmail.com>
2010-12-01 13:41:45 -0800

    Rename sort history alias

--- aliases.sh
+++ aliases.sh
@@ -9,7 +9,7 @@ for ruby in ${rubies[@]}
 do
   alias $ruby="rvm use $ruby"
 done
-alias update_gems="rvm system && gem update && gem cleanup && rvm use 1.8.6 && gem update && gem cleanup && rvm use 1.8.7 && gem update && gem cleanup && rvm use 1.9.1 && gem update && gem cleanup && rvm use 1.9.2 && gem update && gem cleanup && rvm use jruby && gem update && gem cleanup && rvm use rbx && gem update && gem cleanup && rvm use ree && gem update && gem cleanup"
+alias update_gems="rvm system && gem update && yes | gem cleanup && rvm use jruby && gem update && yes | gem cleanup && rvm use rbx && gem update && yes | gem cleanup && rvm use ree && gem update && yes | gem cleanup && rvm use 1.8.6 && gem update && yes | gem cleanup && rvm use 1.8.7 && gem update && yes | gem cleanup && rvm use 1.9.1 && gem update && yes | gem cleanup && rvm use 1.9.2 && gem update && yes | gem cleanup"
 alias update_ports="sudo port selfupdate && sudo port upgrade outdated && sudo port uninstall inactive"
 alias ssbg="/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background"
-alias sort_history="history | cut -c 8- | sort | uniq -c | sort -rn"
+alias history_sort="history | cut -c 8- | sort | uniq -c | sort -rn"
