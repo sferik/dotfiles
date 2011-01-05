a97575456a5961ee3f1a5b65458a30602b30c250
Erik Michaels-Ober <sferik@gmail.com>
2011-01-05 08:24:53 -0800

    Revert "Update macgems"
    This reverts commit 70e161c8359ed48b680e7ff63b3f857d7357bc3e.

--- aliases.sh
+++ aliases.sh
@@ -9,7 +9,7 @@ for ruby in ${rubies[@]}
 do
   alias $ruby="rvm use $ruby"
 done
-alias update_gems="rvm system && sudo gem update && yes | sudo gem cleanup && sudo macgem update && yes | sudo macgem cleanup && rvm use jruby && gem update && yes | gem cleanup && rvm use rbx && gem update && yes | gem cleanup && rvm use ree && gem update && yes | gem cleanup && rvm use 1.8.6 && gem update && yes | gem cleanup && rvm use 1.8.7 && gem update && yes | gem cleanup && rvm use 1.9.1 && gem update && yes | gem cleanup && rvm use 1.9.2 && gem update && yes | gem cleanup"
+alias update_gems="rvm system && sudo gem update && yes | sudo gem cleanup && rvm use jruby && gem update && yes | gem cleanup && rvm use rbx && gem update && yes | gem cleanup && rvm use ree && gem update && yes | gem cleanup && rvm use 1.8.6 && gem update && yes | gem cleanup && rvm use 1.8.7 && gem update && yes | gem cleanup && rvm use 1.9.1 && gem update && yes | gem cleanup && rvm use 1.9.2 && gem update && yes | gem cleanup"
 alias update_ports="sudo port selfupdate && sudo port upgrade outdated && sudo port uninstall inactive"
 alias ssbg="/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background"
 alias history_sort="history | cut -c 8- | sort | uniq -c | sort -rn"
