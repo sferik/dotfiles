affab3cb8d673fd49ef4dd8f88e39dc9045c53bc
Erik Michaels-Ober <sferik@gmail.com>
2011-02-25 10:49:34 -0500

    Add install_gems alias

--- aliases.sh
+++ aliases.sh
@@ -1,28 +1,30 @@
-alias cd..='cd ..'
-alias df='df -h'
-alias du='du -h'
-alias history_sort='history | cut -c 8- | sort | uniq -c | sort -rn'
-alias jobs='jobs -l'
-alias less='less -R'
-alias ll='/bin/ls -AFGHhl'
-alias ls='/bin/ls -FGH'
-alias mkdir='mkdir -p'
-alias more='less'
-alias o='open . &'
-alias reload='source ~/.bashrc'
-alias ssbg='/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background'
-alias su='su -l'
-alias tm='top -o vsize'
-alias tu='top -o cpu'
-alias update_gems='rvm system && sudo gem update && yes | sudo gem cleanup && rvm use jruby && gem update && yes | gem cleanup && rvm use rbx && gem update && yes | gem cleanup && rvm use ree && gem update && yes | gem cleanup && rvm use 1.8.6 && gem update && yes | gem cleanup && rvm use 1.8.7 && gem update && yes | gem cleanup && rvm use 1.9.1 && gem update && yes | gem cleanup && rvm use 1.9.2 && gem update && yes | gem cleanup'
-alias update_ports='sudo port selfupdate && sudo port upgrade outdated && sudo port uninstall inactive'
+alias cd..="cd .."
+alias df="df -h"
+alias du="du -h"
+alias history_sort="history | cut -c 8- | sort | uniq -c | sort -rn"
+alias jobs="jobs -l"
+alias less="less -R"
+alias ll="/bin/ls -AFGHhl"
+alias ls="/bin/ls -FGH"
+alias mkdir="mkdir -p"
+alias more="less"
+alias o="open . &"
+alias reload="source ~/.bashrc"
+alias ssbg="/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background"
+alias su="su -l"
+alias tm="top -o vsize"
+alias tu="top -o cpu"
+default_gems="bundler rak rake rubygems-test wirble"
+alias install_gems="rvm system; sudo gem install $default_gems; rvm use jruby; gem install $default_gems; rvm use rbx; gem install $default_gems; rvm use ree; gem install $default_gems; rvm use 1.8.6; gem install $default_gems; rvm use 1.8.7; gem install $default_gems; rvm use 1.9.1; gem install $default_gems; rvm use 1.9.2; gem install $default_gems"
+alias update_gems="rvm system && sudo gem update && yes | sudo gem cleanup && rvm use jruby && gem update && yes | gem cleanup && rvm use rbx && gem update && yes | gem cleanup && rvm use ree && gem update && yes | gem cleanup && rvm use 1.8.6 && gem update && yes | gem cleanup && rvm use 1.8.7 && gem update && yes | gem cleanup && rvm use 1.9.1 && gem update && yes | gem cleanup && rvm use 1.9.2 && gem update && yes | gem cleanup"
+alias update_ports="sudo port selfupdate && sudo port upgrade outdated && sudo port uninstall inactive"
 
-alias 186='rvm use 1.8.6'
-alias 187='rvm use 1.8.7'
-alias 191='rvm use 1.9.1'
-alias 192='rvm use 1.9.2'
+alias 186="rvm use 1.8.6"
+alias 187="rvm use 1.8.7"
+alias 191="rvm use 1.9.1"
+alias 192="rvm use 1.9.2"
 rubies=(ironruby jruby macruby maglev mput rbx ree)
 for ruby in ${rubies[@]}
 do
-  alias $ruby='rvm use $ruby'
+  alias $ruby="rvm use $ruby"
 done
