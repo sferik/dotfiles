2c7435f69f30596020aff4110b28b20bf311ec3b
Erik Michaels-Ober <sferik@gmail.com>
2011-03-31 00:53:52 -0700

    Lock rvm aliases to specific ruby versions

--- aliases.sh
+++ aliases.sh
@@ -17,14 +17,9 @@ alias tu="top -o cpu"
 default_gems="builder bundler hoe hoe-seattlerb minitest rak rake ruby-graphviz rubygems-test session thor wirble"
 alias install_gems="gem install $default_gems"
 alias update_gems="gem update && yes | gem cleanup"
-
-alias 186="rvm use 1.8.6"
-alias 187="rvm use 1.8.7"
+alias 186="rvm use 1.8.6-p420"
+alias 187="rvm use 1.8.7-p334"
 alias 191="rvm use 1.9.1-p378"
-alias 431="rvm use 1.9.1-p431"
-alias 192="rvm use 1.9.2"
-rubies=(goruby ironruby jruby kiji macruby maglev mput rbx ree)
-for ruby in ${rubies[@]}
-do
-  alias $ruby="rvm use $ruby"
-done
+alias 192="rvm use 1.9.2-p180"
+alias jruby="rvm use jruby-1.6.0"
+alias macruby="rvm use macruby-0.10"
