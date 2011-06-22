aa50f106c3103ba33453ea9838892357a2e1bb81
Erik Michaels-Ober <sferik@gmail.com>
2011-06-22 07:05:54 -0700

    Use tradehill gem instead of mtgox

--- aliases.sh
+++ aliases.sh
@@ -1,7 +1,7 @@
 alias be='bundle exec'
 alias bi='bundle install'
 alias bu='bundle update'
-alias btc='ruby -r rubygems -r mtgox -e "puts MtGox.ticker.last"'
+alias btc='ruby -r rubygems -r tradehill -e "puts TradeHill.ticker.last"'
 alias cd..='cd ..'
 alias df='df -h'
 alias du='du -h'
