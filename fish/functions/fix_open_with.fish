f3e7bee45da4edbb18dc9c39f208eb3f0461390e
Erik Michaels-Ober <sferik@gmail.com>
2013-07-06 09:28:44 +0200

    Fix "Open With"

---
+++ fish/functions/fix_open_with.fish
@@ -0,0 +1,3 @@
+function fix_open_with
+  /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+end
