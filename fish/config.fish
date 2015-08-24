3e8a4b46bebd9dd49b6f8a2758dbbda948426153
Erik Michaels-Ober <sferik@gmail.com>
2015-08-24 20:00:19 +0200

    Export environment variables

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,12 @@
 set -g fish_greeting ''
 
-set EDITOR /usr/local/bin/vim
-set NODE_PATH /usr/local/lib/node_modules
-set PATH /usr/local/bin $PATH
-set PATH /usr/local/sbin $PATH
-set PATH /usr/local/share/npm/bin $PATH
-set PATH $HOME/.rbenv/bin $PATH
-set PATH $HOME/.rbenv/shims $PATH
-set PATH $HOME/bin $PATH
-set RUBYGEMS_EC2_DB1 ec2-54-245-133-190.us-west-2.compute.amazonaws.com
-set RUBYGEMS_EC2_LB1 54.245.255.174
+set -x EDITOR /usr/local/bin/vim
+set -x NODE_PATH /usr/local/lib/node_modules
+set -x PATH /usr/local/bin $PATH
+set -x PATH /usr/local/sbin $PATH
+set -x PATH /usr/local/share/npm/bin $PATH
+set -x PATH $HOME/.rbenv/bin $PATH
+set -x PATH $HOME/.rbenv/shims $PATH
+set -x PATH $HOME/bin $PATH
+set -x RUBYGEMS_EC2_DB1 ec2-54-245-133-190.us-west-2.compute.amazonaws.com
+set -x RUBYGEMS_EC2_LB1 54.245.255.174
