3628a544ffb9a50a7265ca446b9aac51eb51fafc
Erik Michaels-Ober <sferik@gmail.com>
2015-08-28 02:17:17 +0200

    Use any editor, as long as it's vim

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 set -g fish_greeting ''
 
-set -x EDITOR /usr/local/bin/vim
+set -x EDITOR (which vim)
 set -x NODE_PATH /usr/local/lib/node_modules
 set -x PATH /usr/local/bin $PATH
 set -x PATH /usr/local/sbin $PATH
